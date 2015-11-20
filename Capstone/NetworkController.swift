f0630378ba09de998a94cf9c3df13a67fa345a70
Daniel Lee <lee.daniel84@gmail.com>
2015-11-19 19:28:32 -0700

    Views and Controllers updated
    - Restaurant class has been updated

---
+++ Capstone/NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  Capstone
+//
+//  Created by Daniel Lee on 11/19/15.
+//  Copyright © 2015 Daniel Lee. All rights reserved.
+//
+
+import Foundation
--- Capstone/Restaurants.swift
+++ Capstone/Restaurants.swift
@@ -8,3 +8,35 @@
 
 import Foundation
 
+class Restaurants {
+    var name: String
+    var rating: Float
+    var phone: String
+    var categories: String
+    var location: String
+    
+    init(json: [String: AnyObject]) {
+       
+        guard let name = json["name"] as? String,
+        let rating = json["rating"] as? Float,
+        let phone = json["phone"] as? String,
+        let categories = json["categories"] as? String,
+        let location = json["location"] as? String else {
+            
+            self.name = ""
+            self.rating = 0
+            self.phone = ""
+            self.categories = ""
+            self.location = ""
+            return
+        }
+        
+        self.name = name
+        self.rating = rating
+        self.phone = phone
+        self.categories = categories
+        self.location = location
+    }
+    
+}
+
